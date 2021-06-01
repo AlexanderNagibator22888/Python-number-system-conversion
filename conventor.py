0dd256d861bd07052d324aa50583669736af9c2b
AlexanderNagibator22888 <72234818+AlexanderNagibator22888@users.noreply.github.com>
2021-06-01 08:42:07 +0700

    Add files via upload

--- conventor.py
+++ conventor.py
@@ -1,149 +1,136 @@
-#Number system conversion
-#by A. S. Zaykov
-
-import sys
-import math
+from sys import argv
+from math import log
 from pathlib import Path
 
-if len(sys.argv) < 5:
-	sys.exit()
+if len(argv) < 5:
+	exit()
 
 file_name = "conventor.txt"
 name = str(Path(__file__).resolve().parents[0] / file_name)
-
-ux = [ 0, 0 ]
-ue = [ 0, 0 ]
+ux, ue, uu = None, [ None, None ], None
 
 def args(res):
-	f = open(name, "r")
-	temp = -1
+	f, temp = open(name, "r"), 1
 	
 	for i in f:
-		if len(i) < 4 or i[1] != ' ':
-			sys.exit()
-		
+		if len(i) < 5 or i[1] != ' ':
+			exit()
 		if res == i[0]:
 			temp = tuple(i[2::].splitlines())
 			break
 	
 	f.close()
-	if temp != -1:
-		return temp
-	else:
-		sys.exit()
+	if temp == -1:
+		exit()
+	return temp
 
-def uncov():
-	res = 0
-	temp = 0
+def uncov(x1, x2):
+	res, temp = 0, 0
 	
-	for i in sys.argv[3]:
-		temp = ux[0][0].index(i)
+	for i in argv[3]:
+		temp = x1.find(i)
 		if temp == -1:
-			sys.exit()
-		elif temp >= ue[0]:
-			sys.exit()
-		res = res * ue[0] + temp;
-		
+			temp = x2.find(i)
+			if temp == -1:
+				exit()
+		res = res * ue[0] + temp
+	
 	return res
 	
-def conv(num):
-	res = ""
-	temp = 0
+def conv(num, lis):
+	res, temp = "", 0
 	
 	while True:
 		num, temp = divmod(num, ue[1])
-		res += ux[1][0][temp]
+		res += lis[temp]
 		if num == 0:
 			break
 	
-	return res[::-1];
-	
-ns = lambda m, t = 10: int(math.log(m) / math.log(t)) + 1
+	return res[::-1]
+
+ns = lambda m, t = 10: int(log(m) / log(t)) + 1
 
 def f_out(res):
-	temp = [ 1, 1 ]
-	ot = [ "|", "" ]
 	print(res)
+	stemp1, stemp2, temp = "|", "", 1
 	
-	while temp[0] < len(res):
-		if res[temp[0] - 1] == res[temp[0]]:
-			ot[0] += ' '
-			temp[1] += 1
+	for i in range(1, len(res)):
+		if res[i-1] == res[i]:
+			stemp1 += ' '
+			temp += 1
 		else:
-			ot[0] += '|'
-			ot[1] += str(temp[1])
-			temp[1] -= ns(temp[1])
-			for i in range(0, temp[1]):
-				ot[1] += ' '
-			temp[1] = 1
-		temp[0] += 1
-	ot[1] += str(temp[1])
+			stemp1 += '|'
+			stemp2 += str(temp)
+			for j in range(0, temp - ns(temp)):
+				stemp2 += ' '
+			temp = 1
+	stemp2 += str(temp)
 	
-	print(ot[0])
-	print(ot[1])
-	print("Input size =", len(sys.argv[3]))
-	print("Out size =", len(res))
+	print(stemp1)
+	print(stemp2)
+	
+	print("Input size =", len(argv[3]))
+	print("Out Size =", len(res))
 
-def f_out2(res, num, t):
-	temp = str(res) + " ="
-	temp_i = int()
-	temp_s = str(res) + " ="
-	temp_t = int()
+def f_out2(res, les, les2, j, out):
+	stemp1 = str(res) + " ="
+	stemp2 = str(res) + " ="
+	n_temp, p_temp, s_temp = 0, 0, str(j)
 	
 	for i in range(0, len(res)):
-		temp_i = ux[t][0].index(res[i])
-		temp += " " + str(temp_i) + "*" + str(ue[t])
-		temp_t = len(res) - 1 - i
-		temp += "^" + str(temp_t)
-		temp_s += " " + str(temp_i * ue[t] ** temp_t)
-		if (i != len(res) - 1):
-			temp += " +"
-			temp_s += " +"
-	
-	temp_s += " = " + str(num)
-	temp += " = " + str(num)
-	print(temp)
-	print(temp_s)
+		n_temp = les.find(res[i])
+		if n_temp == -1:
+			n_temp = les2.find(res[i])
+		p_temp = len(res) - i - 1
+		if i != 0:
+			stemp1 += " +"
+			stemp2 += " +"
+		stemp1 += ' ' + str(n_temp);
+		stemp1 += '*' + str(p_temp);
+		stemp1 += '^' + s_temp
+		stemp2 += ' ' + str(n_temp * j ** p_temp)
+	print(stemp1, '=', out)
+	print(stemp2, '=', out)
 
 def main():
-	if len(sys.argv[1]) == 3:
-		if sys.argv[1][0] == '-':
-			ux[0] = args(sys.argv[1][1])
-			if sys.argv[1][1] != sys.argv[1][2]:
-				ux[1] = args(sys.argv[1][2])
-			else:
-				ux[1] = ux[0]
+	if len(argv[1]) == 3 and argv[1][0] == '-':
+		ux = args(argv[1][1])
+		ux += tuple(ux[0].lower().splitlines())
+		if argv[1][1] != argv[1][2]:
+			ux += args(argv[1][2])
+			uu = 2
+		else:
+			uu = 0
 	else:
-		sys.exit()
+		exit()
 	
-	if sys.argv[2].isdigit():
-		ue[0] = int(sys.argv[2])
-		if ue[0] > len(ux[0][0]) or ue[0] < 2:
-			sys.exit()
+	if argv[2].isdigit():
+		ue[0] = int(argv[2])
+		if ue[0] > len(ux[0]) or ue[0] < 2:
+			exit()
 	else:
-		sys.exit()
+		exit()
 	
-	if sys.argv[4].isdigit():
-		ue[1] = int(sys.argv[4])
-		if ue[1] > len(ux[1][0]) or ue[1] < 2:
-			sys.exit()
+	if argv[4].isdigit():
+		ue[1] = int(argv[4])
+		if ue[1] > len(ux[uu]) or ue[1] < 2:
+			exit()
 	else:
-		sys.exit()
+		exit()
 	
-	if ue[0] == ue[1] and sys.argv[1][1] == sys.argv[1][2]:
-		sys.exit()
+	if ue[0] == ue[1] and uu == 0:
+		exit()
 	
-	num = uncov()
-	out = conv(num)
+	num = uncov(ux[0], ux[1])
+	out = conv(num, ux[uu])
+	
 	f_out(out)
-	
-	if len(sys.argv) > 5 and sys.argv[5] == "-o":
-		print()
-		print("Вывод преобразованного числа")
-		f_out2(out, num, 1)
-		print()
-		print("Вывод исходного числа")
-		f_out2(sys.argv[3], num, 0)
+	print()
+	print("Вывод исходного числа")
+	f_out2(argv[3], ux[0], ux[1], ue[0], num)
+	print()
+	print("Вывод преобразованеого числа")
+	f_out2(out, ux[uu], ux[1], ue[1], num)
 
 if __name__ == "__main__":
 	main()
